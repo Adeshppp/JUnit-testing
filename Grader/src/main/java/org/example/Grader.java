aac9d169f2d0f552ef83c746b749548a40e66b2c
Adesh <adeshp948@gmail.com>
2023-04-23 07:09:40 -0400

    This commit includes additional edge cases and a new test case that covers situations where the code throws exceptions. These changes are designed to enhance the comprehensiveness of the JUnit testing and ensure that all possible scenarios are thoroughly tested. With these updates, the code is more robust and better equipped to handle a wider range of situations.

--- Grader/src/main/java/org/example/Grader.java
+++ Grader/src/main/java/org/example/Grader.java
@@ -3,10 +3,11 @@ package org.example;
 public class Grader {
     public char determineLetterGrade(int numberGrade){
         if(numberGrade<0) throw new IllegalArgumentException("Number grade can not be negative.");
-        else if(numberGrade<60) return 'F';
-        else if(numberGrade<70) return 'D';
-        else if(numberGrade<80) return 'C';
-        else if(numberGrade<90) return 'B';
+//        if(numberGrade<0) return 'F';// failing case
+        if(numberGrade<60) return 'F';
+        if(numberGrade<70) return 'D';
+        if(numberGrade<80) return 'C';
+        if(numberGrade<90) return 'B';
         return 'A';
 
     }
--- Grader/src/test/java/org/example/GraderTest.java
+++ Grader/src/test/java/org/example/GraderTest.java
@@ -7,9 +7,31 @@ import static org.junit.jupiter.api.Assertions.*;
 class GraderTest {
 
     @Test
+    void negativeGradeShouldReturnIllegalArgumentException(){
+        var grader = new Grader();
+        //lambda expression
+        assertThrows(IllegalArgumentException.class,
+                ()->{
+                    grader.determineLetterGrade(-1);
+                });
+    }
+
+    @Test
+    void zeroShouldReturnF(){
+        var grader = new Grader();
+        assertEquals('F',grader.determineLetterGrade(0));
+    }
+
+    @Test
     void fiftyNineShouldReturnF(){
         var grader = new Grader();
         assertEquals('F',grader.determineLetterGrade(59));
+    }
+
+    @Test
+    void fiftyShouldReturnF(){
+        var grader = new Grader();
+        assertEquals('F',grader.determineLetterGrade(50));
     }
 
     @Test
@@ -18,9 +40,19 @@ class GraderTest {
         assertEquals('D',grader.determineLetterGrade(69));
     }
     @Test
+    void sixtyShouldReturnD(){
+        var grader = new Grader();
+        assertEquals('D',grader.determineLetterGrade(60));
+    }
+    @Test
     void seventyNineShouldReturnC(){
         var grader = new Grader();
         assertEquals('C',grader.determineLetterGrade(79));
+    }
+    @Test
+    void seventyShouldReturnC(){
+        var grader = new Grader();
+        assertEquals('C',grader.determineLetterGrade(70));
     }
     @Test
     void eightyNineShouldReturnB(){
@@ -28,8 +60,18 @@ class GraderTest {
         assertEquals('B',grader.determineLetterGrade(89));
     }
     @Test
+    void eightyShouldReturnB(){
+        var grader = new Grader();
+        assertEquals('B',grader.determineLetterGrade(80));
+    }
+    @Test
     void ninetyNineShouldReturnA(){
         var grader = new Grader();
         assertEquals('A',grader.determineLetterGrade(99));
+    }
+    @Test
+    void ninetyShouldReturnA(){
+        var grader = new Grader();
+        assertEquals('A',grader.determineLetterGrade(90));
     }
 }
